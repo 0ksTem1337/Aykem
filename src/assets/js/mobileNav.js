2f59a35293c826fe77feb0a44530446d8f4e2365
0ksTem1337 <xb3nderx@gmail.com>
2021-10-30 18:58:07 +0300

    version 1.3.0
    mobile sidebar bugs fixed and minor updates

--- src/assets/js/mobileNav.js
+++ src/assets/js/mobileNav.js
@@ -1,13 +1,16 @@
 const burger = document.getElementById('sidebarToggle');
 const sidebar = document.getElementById('sidebar');
+const sidebarPages = document.getElementById('sidebarPages');
 const page = document.getElementById('page');
 const body = document.body;
 
 burger.addEventListener('click', event => {
     if(body.classList.contains('show-sidebar') ) {
         closeSidebar();
+        closeSidebarPages();
     } else {
         showSidebar();
+        showSidebarPages();
     }
 });
 
@@ -19,7 +22,20 @@ function showSidebar() {
     body.classList.add('show-sidebar');
 }
 
+function showSidebarPages() {
+    let mask = document.createElement('div');
+    mask.classList.add('page__mask');
+    mask.addEventListener('click', closeSidebarPages);
+    page.appendChild(mask);
+    body.classList.add('show-sidebar');
+}
+
 function closeSidebar() {
+    body.classList.remove('show-sidebar');
+    document.querySelector('.page__mask').remove();
+}
+
+function closeSidebarPages() {
     body.classList.remove('show-sidebar');
     document.querySelector('.page__mask').remove();
 }
